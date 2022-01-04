04af3a0e80d32cfd2d2f0bdf9cc666f998b48090
mostafa4adel <es-mostafaadel2023@alexu.edu.eg>
2022-01-04 12:55:19 +0200

    DataBase

--- Database/DDL.sql
+++ Database/DDL.sql
@@ -3,23 +3,18 @@ create schema car_registration;
 CREATE TABLE car_registration.user(
     ID INT PRIMARY KEY AUTO_INCREMENT,
     NAME VARCHAR(100) NOT NULL,
-    Email VARCHAR(100) NOT NULL DISTINCT,
-    PASSWORD VARCHAR(100) NOT NULL,
+    Email VARCHAR(100) NOT NULL,
+    Password VARCHAR(100) NOT NULL,
     Sex CHAR(1) NOT NULL CHECK
         (Sex = 'F' OR Sex = 'M'),
         BirthDate DATE,
         Address VARCHAR(1000) NOT NULL,
         PhoneNumber VARCHAR(10)
 );
-
-
 CREATE TABLE car_registration.admin(
     User_Id INT PRIMARY KEY,
     FOREIGN KEY(User_Id) REFERENCES car_registration.user(ID)
-);
-
-
-CREATE TABLE car_registration.car(
+); CREATE TABLE car_registration.car(
     ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     Plate VARCHAR(32),
     Model VARCHAR(32),
@@ -27,9 +22,7 @@ CREATE TABLE car_registration.car(
     Manufacturer VARCHAR(100),
     HorsePower INT,
     ProductionYear YEAR
-);
-
-CREATE TABLE car_registration.rent(
+); CREATE TABLE car_registration.rent(
     user_id INT NOT NULL,
     car_id INT NOT NULL,
     payment CHAR(4) CHECK
